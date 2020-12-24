afa8374c211376712599ae2e5f53e55bfc4ac7aa
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-24 22:44:49 +0900

    Make Board Create

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name="home"),
     path('board', views.board, name="board"),
+    path('board_write', views.board_write, name="board_write"),
+    path('board_insert', views.board_insert, name="board_insert"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from board.models import Board
@@ -12,3 +12,17 @@ def board(request):
     return render(request, "board_list.html", {
         'rsBoard': rsBoard
     })
+
+def board_write(request):
+    return render(request, "board_write.html",)
+
+def board_insert(request):
+    btitle = request.GET['b_title']
+    bnote = request.GET['b_note']
+    bwriter = request.GET['b_writer']
+
+    if btitle != "":
+        rows = Board.objects.create(b_title=btitle, b_note=bnote, b_writer=bwriter)
+        return redirect('/board')
+    else:
+        return redirect('/board_write')
