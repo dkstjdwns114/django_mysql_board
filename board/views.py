42c1806fc38c0cdbe9e167879d94b48716a43a8e
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-24 23:27:31 +0900

    Make board Update

--- board/views.py
+++ board/views.py
@@ -1,6 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from pip._vendor.requests import Response
+
 from board.models import Board
 
 
@@ -8,7 +11,7 @@ def home(request):
     return render(request, "home.html")
 
 def board(request):
-    rsBoard = Board.objects.all()
+    rsBoard = Board.objects.all().order_by('-b_no')
     return render(request, "board_list.html", {
         'rsBoard': rsBoard
     })
@@ -34,4 +37,39 @@ def board_view(request):
     return render(request, "board_view.html", {
         'rsDetail': rsDetail
     })
+
+def board_edit(request):
+    bno = request.GET['b_no']
+    rsDetail = Board.objects.filter(b_no=bno)
+
+    return render(request, "board_edit.html", {
+        'rsDetail': rsDetail
+    })
+
+def board_update(request):
+    bno = request.GET['b_no']
+    btitle = request.GET['b_title']
+    bnote = request.GET['b_note']
+    bwriter = request.GET['b_writer']
+
+    try:
+        board = Board.objects.get(b_no=bno)
+        if btitle != "":
+            board.b_title = btitle
+        if bnote != "":
+            board.b_note = bnote
+        if bwriter != "":
+            board.b_writer = bwriter
+
+        try:
+            board.save()
+            return redirect('/board')
+
+        except ValueError:
+            return Response({"success": False, "msg": "내용을 다시 입력하세요!"})
+
+    except ObjectDoesNotExist:
+        return Response({"success": False, "msg": "게시글이 존재하지 않습니다!"})
+
+
 
